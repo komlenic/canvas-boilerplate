d66b0392a3ce1ac429569af896159afc27ccb574
Chris Komlenic <komlenic@gmail.com>
2022-12-30 10:52:37 -0500

    Add sound class to utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -20,10 +20,33 @@ function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
+class sound {
+
+  #audio;
+
+  constructor(src) {
+    this.#audio = document.createElement('audio');
+    this.#audio.src = src;
+    this.#audio.setAttribute('preload', 'auto');
+    this.#audio.setAttribute('controls', 'none');
+    this.#audio.style.display = 'none';
+    document.body.appendChild(this.#audio);
+  }
+
+  play() {
+    this.#audio.play();
+  }
+
+  pause() {
+    this.#audio.pause();
+  }
+}
+
 module.exports = { 
   randomIntFromRange,
   randomFloatFromRange,
   chance,
   randomItem,
   distance,
+  sound,
 };
