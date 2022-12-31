fdd95ea5cf916c0cd78f20f9d26facaa580e2bde
Chris Komlenic <komlenic@gmail.com>
2022-12-31 14:29:21 -0500

    Apply code standards

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/js/canvas.js',
   output: {
     path: __dirname + '/dist/',
-    filename: './js/canvas.bundle.js'
+    filename: './js/canvas.bundle.js',
   },
   module: {
     rules: [
@@ -16,19 +16,19 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader'
-          }
-        ]
-      }
-    ]
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new BrowserSyncPlugin({
@@ -36,14 +36,14 @@ module.exports = {
       port: 3000,
       server: { baseDir: ['dist'] },
       files: ['./dist/*'],
-      notify: false
+      notify: false,
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       favicon: 'favicon.ico',
-      template: 'src/index.html'
-    })
+      template: 'src/index.html',
+    }),
   ],
   watch: true,
-  devtool: 'source-map'
-}
+  devtool: 'source-map',
+};
